dffbbb991c722ad44a770c94e2c7b571411a9512
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2023-10-30 21:15:56 -0500

    placeholder content updated,

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,26 +1,26 @@
 import "./index.css";
 import background from "../../assets/hero.jpg";
+import web from "../../assets/pexels-luis-gomes-546819.jpg"
+import copy from "../../assets/pexels-mohammad-danish-891059.jpg"
+import graphics from "../../assets/pexels-picjumbocom-196645.jpg"
 
 const index = () => {
   return (
     <div>
       <div className="hero" style={{ backgroundImage: `url(${background})` }}>
         <div className="container">
-          <img src="./icon.png" alt="" />
+          <img src="./icon.png" alt="" className="logo" />
           <h1>Illuminating your visions</h1>
         </div>
       </div>
 
       <div className="container row">
-        <div className="placeholder-img col"></div>
+        <img src={web} alt="image of code" className="col-img col" />
         <div className="col">
           <div className="col-text">
             <h1>web development</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-              laudantium, porro quasi nobis, quis rerum aliquid voluptates
-              itaque natus ad, delectus iure aliquam nostrum aspernatur qui aut
-              a possimus est.
+              Whether you are in the market for a professional website or a dynamic web application, our team brings a blend of functionality and modern design. Lets collaborate to bring your visions to the light.
             </p>
           </div>
         </div>
@@ -29,28 +29,22 @@ const index = () => {
       <div className="container row">
         <div className="col">
           <div className="col-text">
-            <h1>web development</h1>
+            <h1>graphic design</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-              laudantium, porro quasi nobis, quis rerum aliquid voluptates
-              itaque natus ad, delectus iure aliquam nostrum aspernatur qui aut
-              a possimus est.
+              From engaging illustrations to impactful presentation layouts and distinctive digital graphics, our team is skilled in crafting visuals that make an impression.
             </p>
           </div>
         </div>
-        <div className="placeholder-img col"></div>
+        <img src={graphics} alt="image of web design prototypes" className="col-img col" />
       </div>
 
       <div className="container row">
-        <div className="placeholder-img col"></div>
+        <img src={copy} alt="image of notebook and pen" className="col-img col" />
         <div className="col">
           <div className="col-text">
-            <h1>web development</h1>
+            <h1>copywriting</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-              laudantium, porro quasi nobis, quis rerum aliquid voluptates
-              itaque natus ad, delectus iure aliquam nostrum aspernatur qui aut
-              a possimus est.
+              Whether you are in search of persuasive marketing content, informative blog posts, or engaging website materials, our team possesses the expertise to create compelling written pieces.
             </p>
           </div>
         </div>
