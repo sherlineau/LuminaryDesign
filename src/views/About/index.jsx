fc6ce7cd00737aeb620ac6fe4505bd6d80378deb
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2023-11-04 18:57:27 -0500

    about page content added

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -1,11 +1,82 @@
-import './index.css'
+import "./index.css";
 
 const index = () => {
-    return (
-        <div>
-            about
+  return (
+    <div className="about container">
+      <div className="row">
+        <img src="" alt="" className="img-placeholder" />
+        <div className="col">
+          <h1>About Us</h1>
+          <p>
+            Welcome to Luminary Designs, a creative venture born from the love
+            and collaboration of two incredible individuals. We are Linda and
+            Sherline, a couple who have shared a journey of friendship and
+            partnership for the past nine years. Our passion for technology,
+            design, and the written word has led us to create Luminary Designs,
+            a business where we bring our skills, experiences, and creativity to
+            life.
+          </p>
         </div>
-        )
-}
+      </div>
 
-export default index
+      <h1>Our Journey</h1>
+      <p>
+        Linda, with a profound love for writing, is the wordsmith behind our
+        operation. Her background includes writing for magazine articles and
+        business websites, making copywriting a natural fit for her. She excels
+        at crafting words that resonate and connect with audiences.
+      </p>
+      <p>
+        On the other hand, Sherline is a tech enthusiast with a degree in game
+        design and development. While she aspires to forge a career in the
+        gaming industry, she stands by Linda's side to support her and
+        contribute her technical expertise to Luminary Designs.
+      </p>
+
+      <h1>Our Creative Expertise</h1>
+      <p>
+        At Luminary Designs, we offer a diverse range of services. Linda's flair
+        for copywriting ensures your message is not only communicated
+        effectively but also engages and persuades your audience. From web
+        content to promotional materials, Linda's words will breathe life into
+        your ideas.
+      </p>
+      <p>
+        Linda's passion for graphic design is where her artistic skills shine.
+        She loves creating stunning visuals for various occasions, including
+        wedding invitations, flyers, and eye-catching promotional content.
+        Social media decks are also part of her creative repertoire, making your
+        online presence stand out.
+      </p>
+
+      <h1>Technical Prowess</h1>
+      <p>
+        Sherline and Linda work together to provide top-notch web development
+        services. We create fully functional websites tailored to your unique
+        needs. For couples planning their weddings, we even offer the
+        convenience of a QR code for easy access to your personalized wedding
+        site.
+      </p>
+
+      <div className="row">
+        <div className="col">
+          <h1>Our Furry Companions</h1>
+          <p>
+            Apart from our shared love for technology and creativity, we share
+            our lives with five wonderful cats who keep us company and add a
+            dash of charm to our workspace. Luminary Designs is not just a
+            business; it is a representation of our dedication to our craft and
+            our commitment to delivering top-quality work to our clients. We
+            look forward to partnering with you on your creative projects and
+            bringing your vision to life. Thank you for considering Luminary
+            Designs for your web development, graphic design, and copywriting
+            needs.
+          </p>
+        </div>
+        <img src="" alt="" className="img-placeholder" />
+      </div>
+    </div>
+  );
+};
+
+export default index;
