7e363ac47b8734311d88d7ee56999589cc1f87b2
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2023-11-04 21:16:23 -0500

    added emailjs for contact form to send emails

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,57 @@
+import "./index.css";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const index = () => {
+  const form = useRef();
+  const [show, setShow] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7uwnk0v",
+        "template_4tl2uy4",
+        form.current,
+        "05x5X5ZZeIiPOPyHh"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (err) => {
+          console.log(err.text);
+        }
+      );
+
+    setShow(!show);
+  };
+
+  if (show) {
+    return <p>Message sent!</p>;
+  }
+
+  return (
+    <div className="container form">
+      <h2>Send us a message</h2>
+      <form ref={form} onSubmit={sendEmail}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="user_name" id="name" />
+
+        <label htmlFor="email">Email</label>
+        <input type="text" name="user_email" id="email" />
+
+        <label htmlFor="subject">Subject</label>
+        <input type="text" name="subject" id="subject" />
+
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10" />
+
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
+
+export default index;
--- src/views/Contact/index.jsx
+++ src/views/Contact/index.jsx
@@ -1,11 +1,33 @@
-import './index.css'
+import "./index.css";
+import ContactForm from '../../components/ContactForm'
 
 const index = () => {
-    return (
-        <div>
-            contact
-        </div>
-        )
-}
 
-export default index
+  return (
+    <div className="container-50">
+      <h1>Contact Us</h1>
+      <p>
+        At Luminary Designs, we're here to assist you with all your creative
+        needs. Whether you have questions about our services or want to discuss
+        a specific project, we're just a message away. Feel free to reach out to
+        us through email or the following contact form
+      </p>
+
+      <p>
+        Email us at:
+        <a
+          href="mailto:contact@luminarydesigns.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          contact@luminarydesigns.com
+        </a>
+      </p>
+
+    <ContactForm />
+
+    </div>
+  );
+};
+
+export default index;
