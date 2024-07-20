2757b67a52233e6ec62d28b97fd96062e2718f6f
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-07-20 14:11:28 -0500

    added links for site policies

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import About from "./views/About"
 import Home from "./views/Home"
 import Projects from "./views/Projects"
 import Contact from "./views/Contact"
+import DMCA from "./views/DMCA"
+import Privacy from "./views/Privacy"
+import Terms from "./views/Terms"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
@@ -16,6 +19,9 @@ function App() {
         <Route path="/about-us" element={<About/> }/>
         <Route path="/projects" element={<Projects/> }/>
         <Route path="/contact-us" element={<Contact/> }/>
+        <Route path="/dmca-policy" element={<DMCA/> }/>
+        <Route path="/privacy-policy" element={<Privacy/> }/>
+        <Route path="/terms-of-use" element={<Terms/> }/>
       </Routes>
       <Footer />
     </div>
