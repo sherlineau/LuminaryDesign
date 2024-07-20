7a4ecc60daec7f2f8cd637a4dae8209e37ce9a9a
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-07-20 14:11:58 -0500

    content for dmca page

---
+++ src/views/DMCA/index.jsx
@@ -0,0 +1,113 @@
+const index = () => {
+  return (
+    <div className="container policies">
+      <h1>DMCA Policy for Luminary Designs</h1>
+      <div className="container-sm">
+        <p>Effective Date: July 20, 2024 </p>
+        <p>
+          Luminary Designs (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;)
+          respects the intellectual property rights of others and complies with
+          the Digital Millennium Copyright Act (&quot;DMCA&quot;). This DMCA
+          policy outlines our procedures for handling copyright infringement
+          claims and the steps for submitting and responding to DMCA notices.
+        </p>
+
+        <h2>1. Reporting Copyright Infringement</h2>
+        <p>
+          If you believe that your copyrighted work has been copied in a way
+          that constitutes copyright infringement and is accessible on Luminary
+          Designs&apos; website or through our services, please provide a
+          written notification (&quot;DMCA Notice&quot;) containing the
+          following information:
+        </p>
+        <ul>
+          <li>
+            A physical or electronic signature of the copyright owner or a
+            person authorized to act on behalf of the owner.
+          </li>
+          <li>
+            Identification of the copyrighted work claimed to have been
+            infringed.
+          </li>
+          <li>
+            Identification of the material that is claimed to be infringing or
+            to be the subject of infringing activity and that is to be removed
+            or access to which is to be disabled, and information reasonably
+            sufficient to permit us to locate the material.
+          </li>
+          <li>
+            Information reasonably sufficient to permit us to contact you, such
+            as your address, telephone number, and email address.
+          </li>
+          <li>
+            A statement that you have a good faith belief that use of the
+            material in the manner complained of is not authorized by the
+            copyright owner, its agent, or the law.
+          </li>
+          <li>
+            A statement that the information in the notification is accurate,
+            and under penalty of perjury, that you are authorized to act on
+            behalf of the owner of an exclusive right that is allegedly
+            infringed.
+          </li>
+        </ul>
+
+        <h2>2. Counter-Notification</h2>
+        <p>
+          If you believe that material you posted on our site was removed or
+          access to it was disabled by mistake or misidentification, you may
+          file a counter-notification (&quot;Counter Notice&quot;). To be
+          effective, the Counter Notice must be a written communication provided
+          to our DMCA Agent and must contain substantially the following:
+        </p>
+        <ul>
+          <li>Your physical or electronic signature.</li>
+          <li>
+            Identification of the material that has been removed or to which
+            access has been disabled and the location at which the material
+            appeared before it was removed or access disabled.
+          </li>
+          <li>
+            A statement under penalty of perjury that you have a good faith
+            belief that the material was removed or disabled as a result of
+            mistake or misidentification.
+          </li>
+          <li>
+            Your name, address, and telephone number, and a statement that you
+            consent to the jurisdiction of the Federal District Court for the
+            judicial district in which your address is located, or if your
+            address is outside of the United States, for any judicial district
+            in which Luminary Designs may be found, and that you will accept
+            service of process from the person who provided the DMCA Notice or
+            an agent of such person.
+          </li>
+        </ul>
+
+        <h2>3. Repeat Infringers</h2>
+        <p>
+          Luminary Designs reserves the right, in appropriate circumstances, to
+          terminate the accounts of repeat infringers.
+        </p>
+
+        <h2>4. Legal Advice</h2>
+        <p>
+          Please note that nothing in this policy constitutes legal advice. If
+          you have any questions or concerns regarding your rights under the
+          DMCA or applicable law, you should consult a qualified legal
+          professional.
+        </p>
+
+        <h2>5. Changes to This Policy</h2>
+        <p>
+          We may update this DMCA Policy from time to time. We will notify you
+          of any changes by posting the updated policy on our website with an
+          updated effective date.
+        </p>
+
+        <p>This DMCA Policy is effective as of July 20, 2024.</p>
+      </div>
+    </div>
+  );
+};
+
+export default index;
