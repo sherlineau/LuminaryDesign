cad67d02ae02dba755adf079ce79513c98b5d230
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-07-20 14:12:20 -0500

    content for privacy page

---
+++ src/views/Privacy/index.jsx
@@ -0,0 +1,104 @@
+const index = () => {
+  return (
+    <div className="container policies">
+      <h1>Privacy Policy for Luminary Designs</h1>
+      <div className="container-sm">
+        <p>Effective Date: July 20, 2024 </p>
+        <p>
+          Thank you for choosing Luminary Designs for your web development,
+          graphic design, and copywriting needs. Luminary Designs is committed
+          to protecting your privacy. This Privacy Policy outlines how we
+          collect, use, disclose, and safeguard your information when you visit
+          our website or engage our services.
+        </p>
+
+        <h2>1. Information We Collect</h2>
+        <p>
+          We may collect the following types of information when you interact
+          with us:
+        </p>
+        <ul>
+          <li>
+            <span className="bold">Personal Information:</span> Name, email
+            address, phone number, billing address, and other contact
+            information you provide.
+          </li>
+          <li>
+            <span className="bold">Payment Information:</span> Credit card
+            details or other payment information when you make a purchase.
+          </li>
+          <li>
+            <span className="bold">Technical Data: </span> IP address, browser
+            type, operating system, referring URLs, and other technical
+            information collected automatically when you visit our website.
+          </li>
+          <li>
+            <span className="bold">Usage Data:</span> Information about how you
+            use our website, products, and services.
+          </li>
+        </ul>
+
+        <h2>2. How We Use Your Information</h2>
+        <p>We use the information we collect in the following ways:</p>
+        <ul>
+          <li>To provide, operate, and maintain our services.</li>
+          <li>To improve, personalize, and enhance your experience.</li>
+          <li>
+            To process your transactions and send you related information.
+          </li>
+          <li>
+            To communicate with you, including customer service and support.
+          </li>
+          <li>To comply with legal obligations.</li>
+        </ul>
+
+        <h2>3. Sharing Your Information</h2>
+        <p>
+          We do not sell, trade, or otherwise transfer your personal information
+          to outside parties without your consent, except as described in this
+          Privacy Policy or as required by law. We may share your information
+          with:
+        </p>
+        <ul>
+          <li>
+            Service providers and subcontractors who assist us in delivering our
+            services.
+          </li>
+          <li>
+            Third-party analytics and marketing providers to help us improve and
+            promote our services.
+          </li>
+        </ul>
+
+        <h2>4. Security of Your Information</h2>
+        <p>
+          We implement security measures to protect your information from
+          unauthorized access, alteration, disclosure, or destruction. However,
+          please note that no method of transmission over the internet or
+          electronic storage is completely secure.
+        </p>
+
+        <h2>5. Your Choices and Rights</h2>
+        <p>
+          You have the following rights regarding your personal information:
+        </p>
+        <ul>
+          <li><span className="bold">Access and Update:</span>You can access and update your personal information by contacting us.</li>
+          <li><span className="bold">Opt-Out:</span>You can opt out of receiving promotional emails by following the instructions provided in the email.</li>
+          <li><span className="bold">Data Retention:</span> We will retain your information only for as long as necessary to fulfill the purposes outlined in this Privacy Policy unless a longer retention period is required or permitted by law.</li>
+        </ul>
+
+        <h2>6. Children&apos;s Privacy</h2>
+        <p>Our services are not directed to individuals under the age of 18. We do not knowingly collect personal information from children under 18. If you become aware that a child has provided us with personal information, please contact us.</p>
+
+        <h2>7. Changes to This Privacy Policy</h2>
+        <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page with an updated effective date.</p>
+
+        <p>By using our services or accessing our website, you agree to the terms of this Privacy Policy.</p>
+        <p className="bold">Last updated: July 20, 2024</p>
+      </div>
+    </div>
+  );
+};
+
+export default index;
